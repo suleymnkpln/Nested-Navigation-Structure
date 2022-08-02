826083bf07f76a8ddb9164b1b943a3f8d96695e2
hkarpat <suleymnkpln@gmail.com>
2022-08-02 13:33:42 +0300

    Nested-Structure

---
+++ Router.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import Member from './pages/member/Member';
+import MemberDetail from './pages/member/MemberDetail';
+import MemberUpdate from './pages/member/MemberUpdate';
+import Profile from './pages/profile/Profile';
+import ProfileEdit from './pages/profile/ProfileEdit';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const MemberStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="MemberScreen" component={Member} />
+      <Stack.Screen name="MemberDetailScreen" component={MemberDetail} />
+      <Stack.Screen name="MemberUpdateScreen" component={MemberUpdate} />
+    </Stack.Navigator>
+  );
+};
+
+const ProfileStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="ProfileScreen" component={Profile} />
+      <Stack.Screen name="ProfileEditScreen" component={ProfileEdit} />
+    </Stack.Navigator>
+  );
+};
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Profile">
+        <Tab.Screen name="Profile" component={ProfileStack} />
+        <Tab.Screen name="Member" component={MemberStack} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ pages/profile/ProfileEdit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function ProfileEdit() {
+  return (
+    <View>
+      <Text>ProfileEdit</Text>
+    </View>
+  );
+}
+export default ProfileEdit;
